8cdb7367d7d6d866fb27213d51ba7853beb37ab4
husein <huseinmuawiyah@gmail.com>
2026-01-11 23:22:54 +0700

    little change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,36 +36,45 @@ export default function Home() {
 
 
   return (
-    <div className="flex flex-wrap justify-between lg:min-h-[150vh] mx-5 md:mx-20 xl:mx-40 mt-18 gap-5 md:gap-0">
+    <>
+      <div className="flex flex-col justify-between h-screen">
+        <div className="flex flex-wrap justify-between mx-5 md:mx-20 xl:mx-40 mt-18 gap-5 md:gap-0">
 
-      <div className="flex flex-col w-full lg:w-[49%] gap-5">
-        <PriceCategory />
+          <div className="flex flex-col w-full lg:w-[49%] gap-5">
+            <PriceCategory />
 
-        <div className="flex h-18 gap-3">
-          <div className="w-fit h-full aspect-square">
-            <Button variant={'secondary'} className="flex bg-card flex-col gap-2 lg:gap-1 w-full h-full rounded-xl shadow-md transition-none"
-              onClick={() => resetItems()}>
-              <LuRotateCcw className="size-4 lg:size-5" />
-              <span className="text-xs lg:text-base"> Reset </span>
-            </Button>
+            <div className="flex h-18 gap-3">
+              <div className="w-fit h-full aspect-square">
+                <Button variant={'secondary'} className="flex bg-card flex-col gap-2 lg:gap-1 w-full h-full rounded-xl shadow-md transition-none"
+                  onClick={() => resetItems()}>
+                  <LuRotateCcw className="size-4 lg:size-5" />
+                  <span className="text-xs lg:text-base"> Reset </span>
+                </Button>
+              </div>
+
+              <div className="w-full h-full">
+                <AddDevice />
+              </div>
+            </div>
+            {items.length == 0
+              ? <NoDataItemSelected />
+              : <ItemSelected />
+            }
           </div>
 
-          <div className="w-full h-full">
-            <AddDevice />
+          <div className="w-full lg:w-[49%] ">
+            <div className="static md:sticky flex flex-col top-18 gap-5">
+              <PowerEstimation />
+            </div>
           </div>
-        </div>
-        {items.length == 0
-          ? <NoDataItemSelected />
-          : <ItemSelected />
-        }
-      </div>
 
-      <div className="w-full lg:w-[49%] ">
-        <div className="static md:sticky flex flex-col top-18 gap-5">
-          <PowerEstimation />
+
+        </div>
+
+        <div className="w-full">
+          <p className="text-right text-black/50 text-sm mb-5 mx-5 md:mx-20 xl:mx-40 mt-18">@2025 Husein’s Web App Power Calculator</p>
         </div>
       </div>
-
-    </div>
+    </>
   )
 }
